120cdc56ae646e37a4b28f1d237b6ddc5d095334
James Owens <applemaster360@gmail.com>
2013-03-21 14:25:18 -0400

    Added A web View For PDFs

---
+++ HHSApp/HHSApp/MoreDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoreDetailViewController.h
+//  HHSApp
+//
+//  Created by James Owens on 3/21/13.
+//  Copyright (c) 2013 Lordtechy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreDetailViewController : UIViewController
+
+@end
---
+++ HHSApp/HHSApp/MoreDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  MoreDetailViewController.m
+//  HHSApp
+//
+//  Created by James Owens on 3/21/13.
+//  Copyright (c) 2013 Lordtechy. All rights reserved.
+//
+
+#import "MoreDetailViewController.h"
+
+@interface MoreDetailViewController ()
+
+@end
+
+@implementation MoreDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
