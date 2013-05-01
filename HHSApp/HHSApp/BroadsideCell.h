e98132816589322326e3cf841185e45528158324
Connor Koehler <ckoehler@lordtechy.com>
2013-05-01 06:16:39 -0400

    Revert "Fix to stats"
    This reverts commit d15e71fd5fad31db29e1cc751ae694992ef4ce5f.

--- HHSApp/HHSApp/BroadsideCell.h
+++ HHSApp/HHSApp/BroadsideCell.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-@interface BroadsideCell : UITableViewCell{
+#import "GAITrackedViewController.h"
+@interface BroadsideCell : GAITrackedViewController{
     
     UILabel *title;
     UILabel *date;
