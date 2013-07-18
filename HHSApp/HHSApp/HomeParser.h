b6684a3d0975a6c9001ba6c3547258b74dc018c5
Connor Koehler <ckoehler@lordtechy.com>
2013-07-18 14:39:51 -0400

    Changed Copywrite notice in the code
    "Copyright (c) 2013 Hanover High School. All rights reserved."

--- HHSApp/HHSApp/HomeParser.h
+++ HHSApp/HHSApp/HomeParser.h
@@ -3,7 +3,7 @@
 //  HHSApp
 //
 //  Created by Max Greenwald on 6/7/13.
-//  Copyright (c) 2013 Lordtechy. All rights reserved.
+//  Copyright (c) 2013 Hanover High School. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
